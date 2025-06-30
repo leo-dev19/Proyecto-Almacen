2565f5bb4ed92e9ced1afa871a3a4235912f8fed
MilagrosYS <l.mylas10@gmail.com>
2025-06-30 00:48:56 -0500

    firebase y base de datos

--- app/src/main/java/com/example/dorothy/lotes/GestionLotesActivity.kt
+++ app/src/main/java/com/example/dorothy/lotes/GestionLotesActivity.kt
@@ -5,6 +5,7 @@ import android.widget.*
 import android.view.Gravity
 import androidx.appcompat.app.AppCompatActivity
 import com.example.dorothy.R
+import com.google.firebase.firestore.FirebaseFirestore
 
 class GestionLotesActivity : AppCompatActivity() {
     private lateinit var etIdLote: EditText
@@ -18,9 +19,14 @@ class GestionLotesActivity : AppCompatActivity() {
     private lateinit var btnCancelar: Button
     private lateinit var tvProductoInfo: TextView
 
+    private lateinit var db: FirebaseFirestore
+
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.gestion_lotes)
+
+        db = FirebaseFirestore.getInstance()
 
         // Referencias a los campos
         etIdLote = findViewById(R.id.etIdLote)
@@ -51,6 +57,24 @@ class GestionLotesActivity : AppCompatActivity() {
             mostrarToastPersonalizado("Complete todos los campos obligatorios")
             return
         }
+
+        val lote = hashMapOf(
+            "fechaRegistro" to fecha,
+            "tipo" to tipo,
+            "fragil" to fragil,
+            "idDetalle" to idDetalle,
+            "stock" to stock.toIntOrNull(),
+            "fechaVencimiento" to vencimiento
+        )
+
+        db.collection("Lotes").document(idLote).set(lote)
+            .addOnSuccessListener {
+                mostrarToastPersonalizado("Lote guardado en Firebase")
+                limpiarCampos()
+            }
+            .addOnFailureListener { e ->
+                mostrarToastPersonalizado("Error al guardar: ${e.message}")
+            }
 
         // Mostrar los datos en el TextView
         val resumen = """
