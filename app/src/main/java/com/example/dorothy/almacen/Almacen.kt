c9088241a3078017e6d4213bd8328e7c45d02514
NicoSoto04-JAVA <nicolassotelofalla@gmail.com>
2025-07-01 00:29:30 -0500

    hola lo cambie todo

--- app/src/main/java/com/example/dorothy/almacen/Almacen.kt
+++ app/src/main/java/com/example/dorothy/almacen/Almacen.kt
@@ -1,10 +1,11 @@
 package com.example.dorothy.almacen
 
+
 data class Almacen(
-    val id: Int = 0,
-    val nombre: String,
-    val tamano: String,
-    val capacidadMaxima: String,
-    val ubicacion: String,
-    val tipo: String
+    var id: String = "",
+    var nombre: String = "",
+    var tamano: String = "",
+    var capacidadMaxima: String = "",
+    var ubicacion: String = "",
+    var tipo: String = ""
 )
